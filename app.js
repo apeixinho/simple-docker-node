72ab57bee88d23bc7c916ba0f5b1f84b9cbca37e
apeixinho <a_peixinho@sapo.pt>
2017-10-14 19:07:09 +0200

    Bug forget to close string at console log

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!!!')
+  res.send('Hello World!!!');
 })
 
 app.listen(10001, function () {
-  console.log('Example app listening on port 10001)
+  console.log('Example app listening on port 10001');
 })
