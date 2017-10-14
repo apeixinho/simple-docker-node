4042a937fe97a98fc3f049aa3d3692340aef32b0
apeixinho <a_peixinho@sapo.pt>
2017-10-14 19:05:19 +0200

    Removed old http code

--- app.js
+++ app.js
@@ -10,17 +10,3 @@ app.get('/', function (req, res) {
 app.listen(10001, function () {
   console.log('Example app listening on port 10001)
 })
-
-
-// var http = require('http');
-// http.createServer(function(req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.write('<!doctype html>\n<html lang="en">\n' +
-//         '<head>\n<meta charset="utf-8">\n<title>P Web</title>\n' +
-//         '<style type="text/css">* {font-family:arial, sans-serif;}</style>\n' +
-//         '</head>\n<body>\n<h1>Why?</h1>\n' +
-//         '<div id="content"><p>There:</p><ul><li>Because I can</li><li>Why not?!</li><li>Try something new</li><li>For fun ?!?!?</li></ul></div>' +
-//         '\n</body>\n</html>');
-//     res.end();
-// }).listen(10001, 'localhost');
-// console.log('Server running at http://localhost:10001/');
