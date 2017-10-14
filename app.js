2a1e6303f6ff4091d93776d9bd31f84d695d7e9c
apeixinho <a_peixinho@sapo.pt>
2017-10-14 18:58:05 +0200

    Change require from http to express. Changed serve listening port 10001

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 #!/usr/bin/env nodejs
 
-var http = require('http');
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<!doctype html>\n<html lang="en">\n' +
-        '<head>\n<meta charset="utf-8">\n<title>P Web</title>\n' +
-        '<style type="text/css">* {font-family:arial, sans-serif;}</style>\n' +
-        '</head>\n<body>\n<h1>Why?</h1>\n' +
-        '<div id="content"><p>There:</p><ul><li>Because I can</li><li>Why not?!</li><li>Try something new</li><li>For fun ?!?!?</li></ul></div>' +
-        '\n</body>\n</html>');
-    res.end();
-}).listen(10001, 'localhost');
-console.log('Server running at http://localhost:10001/');
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!!!')
+})
+
+app.listen(10001, function () {
+  console.log('Example app listening on port 10001)
+})
+
+
+// var http = require('http');
+// http.createServer(function(req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write('<!doctype html>\n<html lang="en">\n' +
+//         '<head>\n<meta charset="utf-8">\n<title>P Web</title>\n' +
+//         '<style type="text/css">* {font-family:arial, sans-serif;}</style>\n' +
+//         '</head>\n<body>\n<h1>Why?</h1>\n' +
+//         '<div id="content"><p>There:</p><ul><li>Because I can</li><li>Why not?!</li><li>Try something new</li><li>For fun ?!?!?</li></ul></div>' +
+//         '\n</body>\n</html>');
+//     res.end();
+// }).listen(10001, 'localhost');
+// console.log('Server running at http://localhost:10001/');
--- dockerfile
+++ dockerfile
@@ -4,7 +4,7 @@ RUN addgroup -S app && adduser -S -g app app && \
     apk update && apk upgrade && \
     apk add --no-cache build-base && \
     chown -R app:app /home/app && \
-    npm i -g http pm2 && \
+    npm i -g express pm2 && \
     rm -f /var/cache/apk/*
 
 WORKDIR /home/app
@@ -14,5 +14,5 @@ COPY app.js $WORKDIR
 USER app
 
 EXPOSE 10001
- 
+
 CMD ["pm2-docker", "app.js"]
